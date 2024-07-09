f5d66cfd76676489c4345cf988bb555083b78bda
Junhyok <cjh64866@naver.com>
2024-07-09 17:40:22 +0900

    FEAT: 메인 페이지 변경(이미지 추가), Footer 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
-import mainSampleImg from './assets/image/mainsampleimg.png';
-import MainNav from './Pages/MainNav';
+import "./App.css";
+import mainImg from "./assets/image/mainImg.svg";
+import MainNav from "./Pages/MainNav";
+import Footer from "./Pages/Footer";
 
 function App() {
   return (
     <div className="App">
       <MainNav />
       <div className="main_body">
-        <img src={mainSampleImg} alt="Main Img"></img>
+        <img src={mainImg} alt="Main Img"></img>
       </div>
-      <div className="main_footer"> </div>
+      <Footer />
     </div>
   );
 }

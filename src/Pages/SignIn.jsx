ce4166f492ff4a71a457b53171cab1e5e2774a5f
Junhyok <cjh64866@naver.com>
2024-07-11 03:22:36 +0900

    FEAT: 로그인 페이지 로직 & 디자인 완료 (#9)

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,20 +1,36 @@
-import './SignIn.css';
-import MainNav from './MainNav';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import "./SignIn.css";
+import MainNav from "./MainNav";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import RightSide from "../Components/auth/RightSide";
+import Page from "../Components/auth/Page";
+import InputField from "../Components/auth/InputField";
+import BottomBtn from "../Components/auth/BottomBtn";
 
 const User = {
-  id: 'test',
-  pw: '123',
+  id: "test",
+  pw: "123",
 };
 
 function SignIn() {
-  const [id, setId] = useState('');
-  const [pw, setPw] = useState('');
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+
+  const [idPwMatchErrorMessage, setIdPwMatchErrorMessage] = useState(true);
+
+  const [idPwNull, setIdPwNull] = useState(true);
 
   const handleId = (e) => {
     setId(e.target.value);
   };
+
+  useEffect(() => {
+    if (id === "" || pw === "") {
+      setIdPwNull(true);
+    } else {
+      setIdPwNull(false);
+    }
+  }, [id, pw]);
 
   const handlePassword = (e) => {
     setPw(e.target.value);
@@ -22,51 +38,51 @@ function SignIn() {
 
   const onClickConfirmButton = () => {
     if (id === User.id && pw === User.pw) {
-      alert('로그인 성공');
+      setIdPwMatchErrorMessage(true);
+      // 로그인 완료 페이지로 이동 추가
     } else {
-      alert('로그인 실패');
+      setIdPwMatchErrorMessage(false);
     }
   };
 
   return (
-    <div className="Signin">
+    <div className="signIn">
       <MainNav />
-      <div className="page">
-        <div className="titleWrap">로그인</div>
-        <div className="contentWrap">
-          <div className="inputTitle">아이디</div>
-          <div className="inputWrap">
-            <input
-              type="text"
-              className="input"
-              placeholder="podostore"
-              value={id}
-              onChange={handleId}
-            />
-          </div>
-
-          <div style={{ marginTop: '26px' }} className="inputTitle">
-            비밀번호
-          </div>
-          <div className="inputWrap">
-            <input
-              type="password"
-              className="input"
-              placeholder="Lovepodo_S2"
-              value={pw}
-              onChange={handlePassword}
-            />
-          </div>
-        </div>
-        <div>
-          <Link to="/signup">아이디/비밀번호 찾기</Link> |
-          <Link to="/signup"> 회원가입</Link>
-        </div>
-        <div>
-          <button onClick={onClickConfirmButton} className="bottomButton">
-            로그인
-          </button>
-        </div>
+      <div className="box">
+        <div className="left-side"></div>
+        <RightSide>
+          <Page>
+            <div>
+              <div className="titleWrap">로그인 하려고요?</div>
+              <div className="contentWrap">
+                <InputField
+                  title="아이디"
+                  type="text"
+                  placeholder="podostore"
+                  value={id}
+                  onChange={handleId}
+                />
+                <InputField
+                  title="비밀번호"
+                  type="password"
+                  placeholder="Lovepodo_S2"
+                  value={pw}
+                  onChange={handlePassword}
+                  errorMessage="아이디 / 비밀번호 오류"
+                  isValid={idPwMatchErrorMessage}
+                />
+              </div>
+              <div>
+                <BottomBtn onClick={onClickConfirmButton} disabled={idPwNull}>
+                  로그인
+                </BottomBtn>
+              </div>
+              <div className="extraLink">
+                <Link to="/signup">아이디/비밀번호 찾기</Link> | <Link to="/signup"> 회원가입</Link>
+              </div>
+            </div>
+          </Page>
+        </RightSide>
       </div>
     </div>
   );

a1264d33ddcdd5a17374ec0467b6f88b7f7dbddc
Junhyok <cjh64866@naver.com>
2024-07-13 14:32:18 +0900

    REFACTOR: 메인 페이지를 App component와 분리(#24)
    - Main.jsx, Main.css 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import "./App.css";
-import mainImg from "./assets/image/mainImg.svg";
 import MainNav from "./Pages/MainNav";
+import Main from "./Pages/Main";
 import Footer from "./Pages/Footer";
 
 function App() {
   return (
     <div className="App">
       <MainNav />
-      <div className="main_body">
-        <img src={mainImg} alt="Main Img"></img>
-      </div>
+      <Main />
       <Footer />
     </div>
   );

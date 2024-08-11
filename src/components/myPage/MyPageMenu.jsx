be3213ba509f6fc598d7f06b6273717ab371f793
Junhyok <cjh64866@naver.com>
2024-08-12 02:53:19 +0900

    FEAT: 마이페이지 메뉴 routing 추가 (#54)
    작품 관리 페이지 추가 필요

--- src/components/myPage/MyPageMenu.jsx
+++ src/components/myPage/MyPageMenu.jsx
@@ -1,13 +1,20 @@
 import "./MyPageMenu.css";
 import scriptMenuImg from "./../../assets/image/myPage/script.svg";
 import pencilMenuImg from "./../../assets/image/myPage/pencil.svg";
+import { useNavigate } from "react-router-dom";
 
 const MyPageMenu = ({ nickname, currentPage }) => {
+  const navigate = useNavigate();
   return (
     <div className="myPage-menu-side">
       <h1>알맹이 {nickname} 님,</h1>
       <h3>오늘도 달콤한 하루 되세요!</h3>
-      <div className={currentPage === "0" ? "select-menu-btn selected" : "select-menu-btn"}>
+      <div
+        className={currentPage === "0" ? "select-menu-btn selected" : "select-menu-btn"}
+        onClick={() => {
+          navigate("/mypage/purchased");
+        }}
+      >
         <h6>구매한 작품</h6>
         <img src={scriptMenuImg}></img>
       </div>
@@ -15,7 +22,13 @@ const MyPageMenu = ({ nickname, currentPage }) => {
         <h6>작품 관리</h6>
         <img src={pencilMenuImg}></img>
       </div>
-      <p>회원 정보 수정</p>
+      <p
+        onClick={() => {
+          navigate("/mypage/infochange");
+        }}
+      >
+        회원 정보 수정
+      </p>
     </div>
   );
 };

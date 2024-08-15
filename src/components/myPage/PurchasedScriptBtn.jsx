00d4ea59e38def354e6ec0a724064aea455504fc
Junhyok <cjh64866@naver.com>
2024-08-15 21:05:21 +0900

    FEAT: 대본 받기 기능 추가 (#54)
    대본 다운로드 기능 추가 및 API 연결

--- src/components/myPage/PurchasedScriptBtn.jsx
+++ src/components/myPage/PurchasedScriptBtn.jsx
@@ -10,7 +10,7 @@ const ContractStatus = {
   COMPLETE: 3,
 };
 
-const PurchasedScriptBtn = ({ contractStatus, id }) => {
+const PurchasedScriptBtn = ({ contractStatus, id, title }) => {
   const onClickRequestBtn = async () => {
     try {
       const response = await axios.post(
@@ -49,6 +49,31 @@ const PurchasedScriptBtn = ({ contractStatus, id }) => {
     }
   };
 
+  const onClickDownloadScript = async () => {
+    try {
+      const response = await axios.get(`${SERVER_URL}profile/download`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${Cookies.get("accessToken")}`,
+        },
+        params: {
+          id: id,
+        },
+        responseType: "blob",
+      });
+
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", `${title}.pdf`); // 파일명 추출 및 설정
+      document.body.appendChild(link);
+      link.click(); // 링크 클릭으로 다운로드 시작
+      link.remove(); // 링크 요소 제거
+    } catch (error) {
+      alert(error.response.data.error);
+    }
+  };
+
   return (
     <div className="purchased-script-btn">
       {
@@ -70,7 +95,7 @@ const PurchasedScriptBtn = ({ contractStatus, id }) => {
           ),
         }[contractStatus]
       }
-      <button>대본 받기</button>
+      <button onClick={onClickDownloadScript}>대본 받기</button>
     </div>
   );
 };

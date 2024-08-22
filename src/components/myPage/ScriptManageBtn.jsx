3fd1da378c34844b2b01137d726064324e1b05b9
Junhyok <cjh64866@naver.com>
2024-08-23 00:23:47 +0900

    FEAT: 작품 관리 페이지 '심사 중' 상태 추가 (#66)

--- src/components/myPage/ScriptManageBtn.jsx
+++ src/components/myPage/ScriptManageBtn.jsx
@@ -1,7 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import "./ScriptManageBtn.css";
 
-const ScriptManageBtn = ({ scriptSale = false, performSale = false, id }) => {
+const ReviewCompleted = {
+  REVIEW_COMPLETED: true,
+  UNDER_REVIEWING: false,
+};
+
+const ScriptManageBtn = ({ reviewCompleted, scriptSale = false, performSale = false, id }) => {
   const navigate = useNavigate();
 
   const onClickDetail = () => {
@@ -10,21 +15,37 @@ const ScriptManageBtn = ({ scriptSale = false, performSale = false, id }) => {
 
   return (
     <div className="script-manage-btn">
-      <div className="sale-status">
-        {scriptSale ? (
-          <button className="sale-status-enabled">대본 판매 중</button>
-        ) : (
-          <button className="sale-status-disabled">대본 판매 중지</button>
-        )}
-        {performSale ? (
-          <button className="sale-status-enabled">공연권 판매 중</button>
-        ) : (
-          <button className="sale-status-disabled">공연권 판매 중지</button>
-        )}
-      </div>
-      <button id="detail" onClick={onClickDetail}>
-        작품 관리
-      </button>
+      {
+        {
+          [ReviewCompleted.REVIEW_COMPLETED]: (
+            <div className="script-manage-flex">
+              <div className="sale-status">
+                {scriptSale ? (
+                  <button className="sale-status-enabled">대본 판매 중</button>
+                ) : (
+                  <button className="sale-status-disabled">대본 판매 중지</button>
+                )}
+                {performSale ? (
+                  <button className="sale-status-enabled">공연권 판매 중</button>
+                ) : (
+                  <button className="sale-status-disabled">공연권 판매 중지</button>
+                )}
+              </div>
+              <button id="detail" onClick={onClickDetail}>
+                작품 관리
+              </button>
+            </div>
+          ),
+          [ReviewCompleted.UNDER_REVIEWING]: (
+            <div className="script-manage-flex">
+              <div> </div>
+              <button id="detail" disabled="true">
+                심사 중
+              </button>
+            </div>
+          ),
+        }[reviewCompleted]
+      }
     </div>
   );
 };

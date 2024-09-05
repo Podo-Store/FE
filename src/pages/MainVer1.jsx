9d016b5b3710325b6cc2fec950d371bf04979e0e
Junhyok <cjh64866@naver.com>
2024-09-05 11:34:40 +0900

    FEAT: 랜딩 페이지 1차 artwork 반영

---
+++ src/pages/MainVer1.jsx
@@ -0,0 +1,13 @@
+import mainImg from "./../assets/image/mainImgV1.png";
+
+import "./MainVer1.css";
+
+const MainVer1 = () => {
+  return (
+    <div className="d-flex j-content-center a-items-center main-ver-1">
+      <img src={mainImg} alt="mainImg"></img>
+    </div>
+  );
+};
+
+export default MainVer1;

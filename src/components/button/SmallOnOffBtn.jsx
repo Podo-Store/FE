2d0406ceaaedeb1241846ba3b46459ee223d8439
Junhyok <cjh64866@naver.com>
2024-09-30 19:20:09 +0900

    FEAT: 작품 둘러보기 더보기 버튼 추가 및 UI 수정 (#71)
    - 작품 둘러보기 페이지에서 더보기 버튼 추가
    - SmallOnOffBtn component에 style 속성 추가

--- src/components/button/SmallOnOffBtn.jsx
+++ src/components/button/SmallOnOffBtn.jsx
@@ -9,20 +9,28 @@ import "./../../styles/utilities.css";
  * @param {string} props.color - "white", "grey" or "gray", "purple"
  * @param {string} [props.type] - type: "submit", etc.
  * @param {boolean} [props.disabled] - in "purple", disabled
+ * @param {Object} [props.style] - additional style
  * @returns
  */
-const SmallOnOffBtn = ({ text, onClick, type, color, disabled = false }) => {
+const SmallOnOffBtn = ({ text, onClick, type, color, disabled = false, style }) => {
   return color === "white" ? (
     <button
       className="c-pointer p-medium-bold c-main small-on-off-btn"
       id="white"
       onClick={onClick}
       type={type}
+      style={{ ...style }}
     >
       {text}
     </button>
   ) : color === "grey" || color === "gray" ? (
-    <button className="c-pointer small-on-off-btn" id="grey" onClick={onClick} type={type}>
+    <button
+      className="c-pointer small-on-off-btn"
+      id="grey"
+      onClick={onClick}
+      type={type}
+      style={{ ...style }}
+    >
       <p className="p-medium-bold c-white">{text}</p>
     </button>
   ) : (
@@ -32,6 +40,7 @@ const SmallOnOffBtn = ({ text, onClick, type, color, disabled = false }) => {
       onClick={onClick}
       type={type}
       disabled={disabled}
+      style={{ ...style }}
     >
       <p className="p-medium-bold c-white">{text}</p>
     </button>

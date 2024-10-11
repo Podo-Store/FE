63cdc7f76300375a3f3da0df27c4bc97222121d1
pipi-shortstocking <jannie435@naver.com>
2024-10-11 18:57:08 +0900

    fix: ip 주소 변경

--- src/constants/ServerURL.js
+++ src/constants/ServerURL.js
@@ -1,3 +1,3 @@
-// export const SERVER_URL = "http://localhost:8080/";
-export const SERVER_URL = "http://3.36.91.199:8080/";
+export const SERVER_URL = "http://localhost:8080/";
+// export const SERVER_URL = "http://3.36.91.199:8080/";
 // export const SERVER_URL = "http://www.podo-store.com:8080/";

f93b64c874c249499742a379861cbeb5de8834ff
pipi-shortstocking <jannie435@naver.com>
2024-10-16 22:31:19 +0900

    fix: https 설정

--- src/constants/ServerURL.js
+++ src/constants/ServerURL.js
@@ -1,3 +1,3 @@
 // export const SERVER_URL = "http://localhost:8080/";
 // export const SERVER_URL = "http://3.36.91.199:8080/";
-export const SERVER_URL = "https://www.podo-store.com:443/";
+export const SERVER_URL = "https://www.podo-store.com/";

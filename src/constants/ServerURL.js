95ed82edbf815d4d432685561ee54fc5d20eddc3
pipi-shortstocking <jannie435@naver.com>
2024-10-23 14:26:46 +0900

    https 설정

--- src/constants/ServerURL.js
+++ src/constants/ServerURL.js
@@ -1,4 +1,4 @@
 // export const SERVER_URL = "http://localhost:8080/";
 // export const SERVER_URL = "http://13.125.61.13:8080/";
 // export const SERVER_URL = "http://43.201.95.56:8080/";
-export const SERVER_URL = "https://www.pipishortstocking.shop/";
+export const SERVER_URL = "http://www.pipishortstocking.shop/";

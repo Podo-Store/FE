d0d13690aae9fe347c6582c74e6b77203243c54b
Junhyok <cjh64866@naver.com>
2024-10-27 01:05:08 +0900

    FEAT: 랜딩 페이지 수정 전 롤백

--- src/pages/MainVer1.jsx
+++ src/pages/MainVer1.jsx
@@ -1,22 +1,10 @@
 import "./MainVer1.css";
-import firstImage from '../assets/image/LangPageDownArrow.svg';
 
 const MainVer1 = () => {
   return (
-    <div className="background-container">
-      <div id="a1"></div>
-      
-      <div id="title">
-        <div id="purple-rectangle"></div>
-
-        <h1>대본과 공연권 거래<br></br>저희가 도와드릴게요!</h1>
-      </div>
-      
-      <img src={firstImage} alt="First"/>
-    
-    
+    <div className="d-flex j-content-center a-items-center main-ver-1">
+      {/*<img src={mainImg} alt="mainImg"></img>*/}
     </div>
-    
   );
 };
 

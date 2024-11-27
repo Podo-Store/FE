2b8db918e1d81556ab9cc4be07c42eca9476e245
uwol-is-june <std06158@naver.com>
2024-11-27 22:08:14 +0900

    DESIGN: 로딩중 페이지 폰트 및 하단 필요 없는 텍스트 삭제

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -1,6 +1,8 @@
 import { SyncLoader } from "react-spinners";
 import MainNav from "./MainNav";
 import Footer from "./Footer";
+import '../styles/text.css';
+
 
 import "./Loading.css";
 
@@ -10,19 +12,7 @@ const Loading = () => {
       <MainNav />
       <div className="loading-wrap">
         <SyncLoader color="#6b39c0" margin={12} />
-        <p>로딩중</p>
-
-        <div className="loading-content">
-          <p>포도 씨앗 뿌리는 중</p>
-          <p>포도 나무 자라는 중</p>
-          <p>포도 열매 맺히는 중</p>
-        </div>
-
-        <div className="loading-content">
-          <p>토독토독 글 쓰는 중</p>
-          <p>뚝딱뚝딱 공연 만드는 중</p>
-          <p>헤헤히히 관극 가는 중</p>
-        </div>
+        <p className="p-large-bold">로딩중</p>
       </div>
       <Footer />
     </div>

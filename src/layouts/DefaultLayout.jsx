3ee7322acc2cfff9e0a4f9c9954858c4b10fd31b
Junhyok <cjh64866@naver.com>
2025-01-16 17:37:58 +0900

    FEAT: 페이지 이동 시 스크롤 위치를 최상단으로 이동

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,10 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+
 import Footer from "../pages/Footer";
 import MainNav from "../pages/MainNav";
 
 import "./DefaultLayout.css";
 
 const DefaultLayout = () => {
+  // route 이동 시 최상단 이동
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="__default-layout__">
       <MainNav />

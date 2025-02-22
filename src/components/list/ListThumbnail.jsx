288e455cd7e15b9e015955293bf0b90d24e52bcd
Junhyok <cjh64866@naver.com>
2025-02-22 23:50:53 +0900

    FIX: 작품 둘러보기 최대 길이 버그 수정
    한글 분할 형태(NFC)로 인해 문자열 길이 계산이 잘못되는 버그 수정 -> NFD

--- src/components/list/ListThumbnail.jsx
+++ src/components/list/ListThumbnail.jsx
@@ -4,8 +4,12 @@ import ThumbnailImg from "../thumbnail/ThumbnailImg";
 import "./ListThumbnail.css";
 
 function TruncatedText({ text, maxLength }) {
+  // 분할 한글(NFD) -> 완성형 한글(NFC)
+  const normalizedText = text.normalize("NFC");
   // maxLength보다 길면 텍스트를 자르고 "..."을 추가
-  return text.length > maxLength ? text.substring(0, maxLength) + "⋯" : text;
+  return normalizedText.length > maxLength
+    ? normalizedText.substring(0, maxLength) + "⋯"
+    : normalizedText;
 }
 
 const ListThumbnail = ({

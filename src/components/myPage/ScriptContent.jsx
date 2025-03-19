ef0701630596ed94a869650215ecbdf0fc0d6067
Junhyok <cjh64866@naver.com>
2025-03-19 11:25:00 +0900

    REFACTOR: ScriptContent grid 기반으로 수정

--- src/components/myPage/ScriptContent.jsx
+++ src/components/myPage/ScriptContent.jsx
@@ -43,90 +43,90 @@ const ScriptContent = ({
       {items.map((script) => (
         <div key={script.id}>
           <div className="script">
-            <ThumbnailImg imagePath={script.imagePath}></ThumbnailImg>
-            <div className="script-detail">
-              <div className="script-tag">
-                <div
-                  className={`a-items-center ${
-                    currentPage === "1" && script.checked === "PASS" ? "j-content-between" : ""
-                  }`}
-                  id="title"
-                >
-                  <p className="p-large-bold" id="title">
-                    {script.title || "제목 없음"}
-                  </p>
-                  {script.delete && (
-                    <img
-                      id="title-info"
-                      src={circleInfoBtn}
-                      alt="circleInfoBtn"
-                      onClick={() => {
-                        setShowPopup(!showPopup);
-                      }}
-                    />
-                  )}
-                  {showPopup ? <ScriptContentPopup onClose={() => setShowPopup(false)} /> : null}
-                  {/* 작품 관리 페이지 상단 버튼: 심사 끝났을 경우 표시 */}
-                  {currentPage === "1" && script.checked === "PASS" ? (
-                    <ScriptManageTopBtn script={script} />
-                  ) : null}
-                </div>
-                <hr></hr>
-                <p className="p-large-medium" id="author">
-                  {currentPage === "1" ? "" : !script.delete ? script.writer : "삭제된 계정"}
+            <div className="thumbnail-img-wrap">
+              <ThumbnailImg imagePath={script.imagePath}></ThumbnailImg>
+            </div>
+            <div className="script-tag">
+              <div
+                className={`a-items-center ${
+                  currentPage === "1" && script.checked === "PASS" ? "j-content-between" : ""
+                }`}
+                id="title"
+              >
+                <p className="p-large-bold" id="title">
+                  {script.title || "제목 없음"}
                 </p>
-                {currentPage === "1" && script.checked === "WAIT" ? (
-                  // 작품 관리 페이지에서 심사 중인 작품일 경우
-                  <div className="margin-43_4px"></div>
-                ) : currentPage === "0" ? (
-                  // 구매한 작품 페이지
-                  currentTogglePage === "0" ? (
-                    // 구매한 작품 페이지에서 토글 선택이 '대본'일 경우
-                    <PriceText type="script" value={script.scriptPrice || 0} />
-                  ) : currentTogglePage === "1" ? (
-                    // 구매한 작품 페이지에서 토글 선택이 '공연권'일 경우
-                    <PriceText type="perform" value={script.performancePrice || 0} />
-                  ) : null
-                ) : (
-                  // 작품 관리 페이지
-                  <PriceTextsVertical
-                    scriptPrice={script.scriptPrice || 0}
-                    performPrice={script.performancePrice || 0}
+                {script.delete && (
+                  <img
+                    id="title-info"
+                    src={circleInfoBtn}
+                    alt="circleInfoBtn"
+                    onClick={() => {
+                      setShowPopup(!showPopup);
+                    }}
                   />
                 )}
+                {showPopup ? <ScriptContentPopup onClose={() => setShowPopup(false)} /> : null}
+                {/* 작품 관리 페이지 상단 버튼: 심사 끝났을 경우 표시 */}
+                {currentPage === "1" && script.checked === "PASS" ? (
+                  <ScriptManageTopBtn script={script} />
+                ) : null}
               </div>
-              {/* Button: props */}
-              {currentPage === "0" && !script.delete ? (
-                // 구매한 작품 페이지 (PurchasedScript.jsx)
+              <hr></hr>
+              <p className="p-large-medium" id="author">
+                {currentPage === "1" ? "" : !script.delete ? script.writer : "삭제된 계정"}
+              </p>
+              {currentPage === "1" && script.checked === "WAIT" ? (
+                // 작품 관리 페이지에서 심사 중인 작품일 경우
+                <div className="margin-43_4px"></div>
+              ) : currentPage === "0" ? (
+                // 구매한 작품 페이지
                 currentTogglePage === "0" ? (
-                  // PurchasedScriptBtn.jsx
-                  <Button
-                    purchaseStatus={[script.script, script.performance]}
-                    id={script.id}
-                    title={script.title}
-                    productId={script.productId}
-                    buyPerformance={script.buyPerformance}
-                    orderStatus={script.orderStatus}
-                  />
-                ) : (
-                  // PurchasedPerformBtn.jsx
-                  <Button
-                    id={script.id}
-                    possibleCount={script.possibleCount}
-                    orderStatus={script.orderStatus}
-                  />
-                )
-              ) : currentPage === "1" && !script.delete ? (
-                // 작품 관리 페이지 ScriptManageBtn.jsx
-                // checked - false: 심사 중, true: 심사 완료
-                <Button
-                  reviewCompleted={script.checked}
-                  scriptSale={script.script}
-                  performSale={script.performance}
-                  id={script.id}
+                  // 구매한 작품 페이지에서 토글 선택이 '대본'일 경우
+                  <PriceText type="script" value={script.scriptPrice || 0} />
+                ) : currentTogglePage === "1" ? (
+                  // 구매한 작품 페이지에서 토글 선택이 '공연권'일 경우
+                  <PriceText type="perform" value={script.performancePrice || 0} />
+                ) : null
+              ) : (
+                // 작품 관리 페이지
+                <PriceTextsVertical
+                  scriptPrice={script.scriptPrice || 0}
+                  performPrice={script.performancePrice || 0}
                 />
-              ) : null}
+              )}
             </div>
+            {/* Button: props */}
+            {currentPage === "0" && !script.delete ? (
+              // 구매한 작품 페이지 (PurchasedScript.jsx)
+              currentTogglePage === "0" ? (
+                // PurchasedScriptBtn.jsx
+                <Button
+                  purchaseStatus={[script.script, script.performance]}
+                  id={script.id}
+                  title={script.title}
+                  productId={script.productId}
+                  buyPerformance={script.buyPerformance}
+                  orderStatus={script.orderStatus}
+                />
+              ) : (
+                // PurchasedPerformBtn.jsx
+                <Button
+                  id={script.id}
+                  possibleCount={script.possibleCount}
+                  orderStatus={script.orderStatus}
+                />
+              )
+            ) : currentPage === "1" && !script.delete ? (
+              // 작품 관리 페이지 ScriptManageBtn.jsx
+              // checked - false: 심사 중, true: 심사 완료
+              <Button
+                reviewCompleted={script.checked}
+                scriptSale={script.script}
+                performSale={script.performance}
+                id={script.id}
+              />
+            ) : null}
           </div>
         </div>
       ))}

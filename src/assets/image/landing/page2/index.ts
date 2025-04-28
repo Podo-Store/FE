344699264bc6c19051f823954f2cd30460dc480d
Junhyeok <cjh64866@naver.com>
2025-04-28 18:20:13 +0900

    feat: 랜딩페이지2 768 전체 480 일부 (#182)

--- src/assets/image/landing/page2/index.ts
+++ src/assets/image/landing/page2/index.ts
@@ -2,5 +2,8 @@ export { default as page2ButtonImg } from "./degree45Arrow.svg";
 export { default as title } from "./title.svg";
 export { default as title_right } from "./title_right.svg";
 export { default as content1 } from "./content1/신난포 1 1.png";
+export { default as content1_768 } from "./content1/content1_768.png";
 export { default as content2 } from "./content2/신난도_1_bg_removed.png 1.png";
+export { default as content2_768 } from "./content2/content2_768.png";
 export { default as content3 } from "./content3/신난포도 1 1.png";
+export { default as content3_768 } from "./content3/content3_768.png";

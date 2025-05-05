41183d78c0fbd04bda044b9b2cd4e7ce3a18e1fe
Junhyeok <cjh64866@naver.com>
2025-05-06 01:43:44 +0900

    feat: 작품 관리 페이지 상단 버튼 disabled 상태 추가

--- src/components/myPage/ScriptManageEachTopBtn.tsx
+++ src/components/myPage/ScriptManageEachTopBtn.tsx
@@ -1,12 +1,31 @@
+import clsx from "clsx";
 import { PropsWithChildren } from "react";
 
 import ic_outline_check_circle from "./../../assets/image/myPage/ic_outline_check_circle.svg";
+import ic_outline_disabled_circle from "./../../assets/image/myPage/ic_outline_disabled_circle.svg";
 
-const ScriptManageEachTopBtn: React.FC<PropsWithChildren> = ({ children }) => {
+interface ScriptManageEachTopBtnProps extends PropsWithChildren {
+  disabled?: boolean;
+}
+
+const ScriptManageEachTopBtn: React.FC<ScriptManageEachTopBtnProps> = ({
+  children,
+  disabled = false,
+}) => {
+  const baseClassName =
+    "flex justify-center items-center gap-[5px] w-[100px] h-[32px] rounded-[50px] box-border cursor-default";
+  const postClassName = !disabled
+    ? "border border-solid border-(--Main) bg-(--purple10)"
+    : "bg-(--grey4)";
+
   return (
-    <div className="flex justify-center items-center gap-[5px] w-[100px] h-[32px] rounded-[50px] border border-solid border-(--Main) bg-(--purple10) cursor-default">
-      <img className="size-[15px]" src={ic_outline_check_circle} alt="check"></img>
-      <p className="p-xs-bold text-(--Main)">{children}</p>
+    <div className={clsx(baseClassName, postClassName)}>
+      <img
+        className="size-[15px]"
+        src={!disabled ? ic_outline_check_circle : ic_outline_disabled_circle}
+        alt="check"
+      ></img>
+      <p className={clsx("p-xs-bold", !disabled ? "text-(--Main)" : "text-[#fff]")}>{children}</p>
     </div>
   );
 };

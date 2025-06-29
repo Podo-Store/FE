155633682779c7faa879ba6b0b918fd1fcd6b80a
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-06-30 04:21:58 +0900

    feat: 별점 기능 추가

--- src/pages/work/review/reviewWrite.tsx
+++ src/pages/work/review/reviewWrite.tsx
@@ -7,8 +7,14 @@ import SmallOnOffBtn from "@/components/button/RoundBtn_135_40";
 import "./reviewWrite.scss";
 const reviewWrite = () => {
   const { id } = useParams<string>();
-
+  const [text, setText] = useState("");
+  const [selectedStar, setSelectedStar] = useState(0);
   const navigate = useNavigate();
+
+  const handleChange = (e: any) => {
+    setText(e.target.value);
+  };
+
   return (
     <div className="mx-auto all pb-[92px]">
       <div className="mt-[37px]  flex  flex-col  gap-[14px]">
@@ -45,25 +51,49 @@ const reviewWrite = () => {
                 <div className="flex flex-row gap-[15px]">
                   <div className="flex flex-row gap-[2px]">
                     {Array.from({ length: 5 }, (_, i) => (
-                      <div key={i}>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="25"
-                          height="25"
-                          viewBox="0 0 25 25"
-                          fill="none"
-                        >
-                          <path
-                            d="M22.1694 8.6211L15.9707 7.72022L13.1997 2.10255C13.124 1.94874 12.9995 1.82423 12.8457 1.74854C12.4599 1.55811 11.9912 1.71681 11.7983 2.10255L9.02731 7.72022L2.82858 8.6211C2.65768 8.64552 2.50143 8.72608 2.3818 8.84815C2.23717 8.9968 2.15748 9.19679 2.16022 9.40416C2.16297 9.61154 2.24794 9.80935 2.39645 9.95411L6.88131 14.3267L5.82174 20.501C5.79689 20.6446 5.81279 20.7923 5.86762 20.9274C5.92245 21.0624 6.01403 21.1794 6.13196 21.2651C6.2499 21.3507 6.38948 21.4016 6.53487 21.412C6.68027 21.4224 6.82565 21.3918 6.95455 21.3237L12.499 18.4087L18.0434 21.3237C18.1948 21.4043 18.3706 21.4312 18.539 21.4019C18.9638 21.3286 19.2495 20.9258 19.1762 20.501L18.1167 14.3267L22.6015 9.95411C22.7236 9.83448 22.8042 9.67823 22.8286 9.50733C22.8945 9.08009 22.5966 8.68458 22.1694 8.6211ZM16.2295 13.7114L17.1108 18.8457L12.499 16.4238L7.88717 18.8482L8.76852 13.7139L5.03805 10.0762L10.1943 9.32667L12.499 4.65626L14.8037 9.32667L19.9599 10.0762L16.2295 13.7114Z"
-                            fill={"#BABABA"}
-                          />
-                        </svg>
-                        {/* fill="#6A39C0" */}
+                      <div
+                        key={i}
+                        onClick={() => {
+                          if (selectedStar === i + 1) {
+                            setSelectedStar(i);
+                          } else {
+                            setSelectedStar(i + 1);
+                          }
+                        }}
+                        className="cursor-pointer"
+                      >
+                        {i >= selectedStar ? (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="25"
+                            height="25"
+                            viewBox="0 0 25 25"
+                            fill="none"
+                          >
+                            <path
+                              d="M22.1694 8.6211L15.9707 7.72022L13.1997 2.10255C13.124 1.94874 12.9995 1.82423 12.8457 1.74854C12.4599 1.55811 11.9912 1.71681 11.7983 2.10255L9.02731 7.72022L2.82858 8.6211C2.65768 8.64552 2.50143 8.72608 2.3818 8.84815C2.23717 8.9968 2.15748 9.19679 2.16022 9.40416C2.16297 9.61154 2.24794 9.80935 2.39645 9.95411L6.88131 14.3267L5.82174 20.501C5.79689 20.6446 5.81279 20.7923 5.86762 20.9274C5.92245 21.0624 6.01403 21.1794 6.13196 21.2651C6.2499 21.3507 6.38948 21.4016 6.53487 21.412C6.68027 21.4224 6.82565 21.3918 6.95455 21.3237L12.499 18.4087L18.0434 21.3237C18.1948 21.4043 18.3706 21.4312 18.539 21.4019C18.9638 21.3286 19.2495 20.9258 19.1762 20.501L18.1167 14.3267L22.6015 9.95411C22.7236 9.83448 22.8042 9.67823 22.8286 9.50733C22.8945 9.08009 22.5966 8.68458 22.1694 8.6211ZM16.2295 13.7114L17.1108 18.8457L12.499 16.4238L7.88717 18.8482L8.76852 13.7139L5.03805 10.0762L10.1943 9.32667L12.499 4.65626L14.8037 9.32667L19.9599 10.0762L16.2295 13.7114Z"
+                              fill="#BABABA"
+                            />
+                          </svg>
+                        ) : (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="25"
+                            height="25"
+                            viewBox="0 0 25 25"
+                            fill="none"
+                          >
+                            <path
+                              d="M22.1694 8.6211L15.9707 7.72022L13.1997 2.10255C13.124 1.94874 12.9995 1.82423 12.8457 1.74854C12.4599 1.55811 11.9912 1.71681 11.7983 2.10255L9.02731 7.72022L2.82858 8.6211C2.65768 8.64552 2.50143 8.72608 2.3818 8.84815C2.23717 8.9968 2.15748 9.19679 2.16022 9.40416C2.16297 9.61154 2.24794 9.80935 2.39645 9.95411L6.88131 14.3267L5.82174 20.501C5.79689 20.6446 5.81279 20.7923 5.86762 20.9274C5.92245 21.0624 6.01403 21.1794 6.13196 21.2651C6.2499 21.3507 6.38948 21.4016 6.53487 21.412C6.68027 21.4224 6.82565 21.3918 6.95455 21.3237L12.499 18.4087L18.0434 21.3237C18.1948 21.4043 18.3706 21.4312 18.539 21.4019C18.9638 21.3286 19.2495 20.9258 19.1762 20.501L18.1167 14.3267L22.6015 9.95411C22.7236 9.83448 22.8042 9.67823 22.8286 9.50733C22.8945 9.08009 22.5966 8.68458 22.1694 8.6211Z"
+                              fill="#6A39C0"
+                            />
+                          </svg>
+                        )}
                       </div>
                     ))}
                   </div>
                   <span className="flex flex-row gap-[9px]">
-                    <p className="p-large-bold">4</p>
+                    <p className="p-large-bold">{selectedStar}</p>
                     <p className="p-medium-regular">/ 5</p>
                   </span>
                 </div>
@@ -86,12 +116,17 @@ const reviewWrite = () => {
           <div className="flex flex-col">
             <span className="p-large-bold mb-[10px]"> 내용 작성</span>
             <div className="flex flex-col border-[0.5px] rounded-[5px] h-[250px] ">
-              <textarea className=" p-medeim-regular h-[203px] p-[20px] border-none box-border resize-none  rounded-[5px] focus:outline-none focus:ring-0 "></textarea>
+              <textarea
+                className=" p-medeim-regular h-[203px] p-[20px] border-none box-border resize-none  rounded-[5px] focus:outline-none focus:ring-0 "
+                onChange={handleChange}
+              ></textarea>
               <span className="h-[47px] bg-[var(--purple10)] w-full flex flex-row justify-between  rounded-[5px] ">
                 <p className="flex my-auto ml-[20px] w-fit p-medium-regular">
                   EX) 내용이 재밌었요!
                 </p>
-                <span className="my-auto mr-[13px]">0 / 50자 이상</span>
+                <span className="my-auto mr-[13px]">
+                  {text.length} / 50자 이상
+                </span>
               </span>
             </div>
           </div>
@@ -113,6 +148,7 @@ const reviewWrite = () => {
           </SmallOnOffBtn>
           <SmallOnOffBtn
             color="purple"
+            disabled={text.length < 50}
             onClick={() => navigate("/mypage/scriptmanage")}
           >
             작성하기

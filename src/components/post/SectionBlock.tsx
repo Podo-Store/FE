6059e69b595bf89fb158dc144093e29ee6c6443f
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-07-21 03:06:49 +0900

    feat: 작품 둘러보기 - 320 적용 (#276)

--- src/components/post/SectionBlock.tsx
+++ src/components/post/SectionBlock.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { PostCardPreview } from "@/components/post/PostList";
 import { ScriptItem } from "@/api/user/postListApi";
+import useWindowDimensions from "@/hooks/useWindowDimensions";
 
 interface Props {
   posts: ScriptItem[];
@@ -23,16 +23,29 @@ const SectionBlock = ({
   onToggleLike,
   isLikePage = false,
 }: Props) => {
+  const { widthConditions } = useWindowDimensions();
+  const { isSmallMobile } = widthConditions;
+
   return (
     <section>
       <div className="flex justify-between mb-[24px]">
         {" "}
-        <h2 className="h5-medium">{title}극</h2>
+        <h2 className={`${isSmallMobile ? "p-small-medium" : "h5-medium"}`}>
+          {title}극
+        </h2>
         <button
-          className="flex px-[25px] py-[3px] justify-center items-center rounded-[30px] border-[2.5px] border-[#6A39C0] bg-[#FFF] hover:bg-[var(--purple9)] active:border-[var(--purple9)]"
+          className={`flex justify-center items-center rounded-[30px] border-[2.5px] border-[#6A39C0] bg-[#FFF] hover:bg-[var(--purple9)] active:border-[var(--purple9)] ${
+            isSmallMobile ? " px-[15px] py-[3px]" : " px-[25px] py-[3px]"
+          }`}
           onClick={() => onMoreClick(title)}
         >
-          <p className="text-center p-small-bold text-[#6A39C0] ">더보기</p>
+          <p
+            className={`text-center  text-[#6A39C0] ${
+              isSmallMobile ? " p-xs-bold" : " p-small-bold"
+            }`}
+          >
+            더보기
+          </p>
         </button>
       </div>
 
@@ -46,7 +59,11 @@ const SectionBlock = ({
         />
       ) : posts.length === 0 ? (
         <div>
-          <p className="m-auto w-fit p-large-bold">
+          <p
+            className={`m-auto w-fit p-large-bold ${
+              isSmallMobile ? " p-small-bold" : "p-large-bold"
+            }`}
+          >
             {isLikePage ? "좋아한 작품이 없습니다." : "등록된 작품이 없습니다."}
           </p>
         </div>
--- src/components/post/ViewToggleButton.tsx
+++ src/components/post/ViewToggleButton.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-
 import cardView from "./../../assets/image/post/list/ic_card_view.svg";
 import gridView from "./../../assets/image/post/list/ic_grid_view.svg";
+
+import "./viewToggleButton.scss";
 
 interface Props {
   viewType: "grid" | "card";
@@ -18,7 +18,7 @@ const ViewToggleButton = ({ viewType, setViewType }: Props) => {
       src={viewType === "grid" ? gridView : cardView}
       alt={viewType === "grid" ? "그리드 보기로 전환" : "카드 보기로 전환"}
       onClick={toggleViewType}
-      className="cursor-pointer hover:bg-[#f4f2f3]"
+      className="cursor-pointer hover:bg-[#f4f2f3] view-toggle"
     />
   );
 };

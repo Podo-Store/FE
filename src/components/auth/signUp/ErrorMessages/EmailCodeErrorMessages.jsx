93c1f4515fd27915910320b144fedb247f492f87
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-08-11 01:52:34 +0900

    feat: 아이디/비밀번호 찾기 320적용 (#286)
    * feat: 아이디찾기/비밀번호찾기 토글 바 320적용
    
    * feat: 아이디/비밀번호 찾기 320적용

--- src/components/auth/signUp/ErrorMessages/EmailCodeErrorMessages.jsx
+++ src/components/auth/signUp/ErrorMessages/EmailCodeErrorMessages.jsx
@@ -1,20 +1,35 @@
 import { CheckerMessage, ErrorMessage } from "..";
-
-const EmailCodeErrorMessages = ({ emailCodeChecker, emailSended, onClickEmailSend }) => {
+import useWindowDimensions from "@/hooks/useWindowDimensions";
+const EmailCodeErrorMessages = ({
+  emailCodeChecker,
+  emailSended,
+  onClickEmailSend,
+}) => {
+  const { isSmallMobile } = useWindowDimensions().widthConditions;
   return (
     <div className="f-dir-column" id="error-wrap">
       <div className="j-content-between">
         {emailCodeChecker.show ? (
           emailCodeChecker.match ? (
-            <CheckerMessage checkedFlag={true} message="인증이 완료되었습니다." />
+            <CheckerMessage
+              checkedFlag={true}
+              message="인증이 완료되었습니다."
+              smallMessage={isSmallMobile ? true : false}
+            />
           ) : (
-            <ErrorMessage message="인증번호가 정확하지 않습니다." />
+            <ErrorMessage
+              message="인증번호가 정확하지 않습니다."
+              smallMessage={isSmallMobile ? true : false}
+            />
           )
         ) : (
           <p></p>
         )}
         {emailSended ? (
-          <p className="p-xs-under c-grey4 c-pointer" onClick={onClickEmailSend}>
+          <p
+            className="p-xs-under c-grey4 c-pointer"
+            onClick={onClickEmailSend}
+          >
             메일 다시 보내기
           </p>
         ) : null}

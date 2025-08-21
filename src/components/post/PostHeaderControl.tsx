e600d86c25d840877093c378363aa5b33d4253ae
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-08-21 21:44:51 +0900

    feat: stage 설명 띠 추가

--- src/components/post/PostHeaderControl.tsx
+++ src/components/post/PostHeaderControl.tsx
@@ -4,9 +4,10 @@ import StageTab from "./StageTab";
 import ViewToggleButton from "./ViewToggleButton";
 import StoryLengthTeb from "@/components/post/StoryLengthTabs";
 import SortDropdown from "@/components/post/SortDropdown";
+import { StageType } from "@/types/stage";
 
 interface PostHeaderControlProps {
-  activeStage: string;
+  activeStage: StageType;
   setActiveStage: (value: string) => void;
   activeStoryLength: string;
   setActiveStoryLength: (value: string) => void;
@@ -17,7 +18,15 @@ interface PostHeaderControlProps {
   setSortType?: (value: "POPULAR" | "LIKE_COUNT" | "LATEST") => void;
   stageBottomBorderWidth: string;
   stageIcon?: boolean;
+  stageBelt?: boolean;
 }
+
+const STAGE_BELT: Record<StageType, string> = {
+  포도밭: "모든 작가의 작품을 볼 수 있어요.",
+  포도알: "포도상점에 첫 발을 내딛은 작가들의 작품을 볼 수 있어요.",
+  포도송이: "창작 활동을 활발히 이어가는 작가들의 작품을 볼 수 있어요.",
+  와인: "오랜 경험으로 완성도를 높인 작가들의 작품을 감상할 수 있어요.",
+};
 
 const PostHeaderControl: React.FC<PostHeaderControlProps> = ({
   activeStage,
@@ -31,9 +40,10 @@ const PostHeaderControl: React.FC<PostHeaderControlProps> = ({
   setSortType,
   stageBottomBorderWidth,
   stageIcon,
+  stageBelt = false,
 }) => {
   return (
-    <>
+    <div className="mb-[35px]">
       {/*----- 스테이지 메뉴 -----*/}
       <div className="relative ">
         <StageTab
@@ -46,7 +56,7 @@ const PostHeaderControl: React.FC<PostHeaderControlProps> = ({
         ></span>
       </div>
       {/*----- 카테고리 메뉴 -----*/}
-      <div className="flex items-center justify-between w-full mb-[35px] ">
+      <div className="flex items-center justify-between w-full ">
         <StoryLengthTeb
           activeStoryLength={activeStoryLength}
           setActiveStoryLength={setActiveStoryLength}
@@ -62,7 +72,19 @@ const PostHeaderControl: React.FC<PostHeaderControlProps> = ({
           <ViewToggleButton viewType={viewType} setViewType={setViewType} />
         </div>
       </div>
-    </>
+      {stageBelt && (
+        <div className="relative h-[44px] ">
+          <div
+            className={`flex absolute left-1/2 -translate-x-1/2 ${stageBottomBorderWidth} bg-[var(--grey1)] shadow-custom-inset h-[30px] sm:h-[33px] md:h-[44px] p-xs-medium sm:p-small-medium md:p-large-medium  items-center justify-center`}
+          >
+            <span className="text-[var(--purple4)]">{activeStage}</span>
+            {`에서는 ${
+              STAGE_BELT[activeStage as keyof typeof STAGE_BELT] ?? ""
+            }`}
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
---
+++ src/types/stage.ts
@@ -0,0 +1,1 @@
+export type StageType = "포도밭" | "포도알" | "포도송이" | "와인";

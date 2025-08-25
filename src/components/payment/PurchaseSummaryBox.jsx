cb7516e685d0407965e13efb0b88c7bc938e0366
Junhyeok <cjh64866@naver.com>
2025-08-25 20:57:02 +0900

    feat: 결제 페이지 320px 반응형 디자인 적용

--- src/components/payment/PurchaseSummaryBox.jsx
+++ src/components/payment/PurchaseSummaryBox.jsx
@@ -30,40 +30,49 @@ const PurchaseSummaryBox = ({
     <div className="purchase-summary-box" style={style} {...props}>
       {page === 0 ? (
         // 구매 페이지
-        <h4 className="p-large-bold">{title}</h4>
+        <h4 className="p-medium-bold sm:p-large-bold">{title}</h4>
       ) : (
         // 구매 완료 페이지
         <h4 className="h5-bold">{title}</h4>
       )}
       <div style={{ height: "11.6px" }}></div>
       <div className={`price-wrap ${setLeftPadding ? "left-padding" : ""}`}>
-        <p className="p-medium-regular">
-          {page === 0 ? "대본 가격" : `대본 ${scriptTitle} ${buyScript ? 1 : "-"} 개`}
+        <p className="p-small-regular sm:p-medium-regular">
+          {page === 0
+            ? "대본 가격"
+            : `대본 ${scriptTitle} ${buyScript ? 1 : "-"} 개`}
         </p>
-        <p className="p-medium-regular">{buyScript ? formatPrice(scriptPrice) : "-"} 원</p>
+        <p className="p-small-regular sm:p-medium-regular">
+          {buyScript ? formatPrice(scriptPrice) : "-"} 원
+        </p>
       </div>
       <div className={`price-wrap ${setLeftPadding ? "left-padding" : ""}`}>
-        <p className="p-medium-regular">
+        <p className="p-small-regular sm:p-medium-regular">
           {page === 0
             ? "공연권 가격"
             : `공연권 ${scriptTitle} ${buyPerform ? performAmount : "-"} 개`}
         </p>
-        <p className="p-medium-regular">
+        <p className="p-small-regular sm:p-medium-regular">
           {buyPerform ? formatPrice(performPrice * performAmount) : "-"} 원
         </p>
       </div>
       <hr
         style={
           page === 0
-            ? { marginTop: "12.08px", marginBottom: "14.92px", marginRight: "12.97px" }
+            ? {
+                marginTop: "12.08px",
+                marginBottom: "14.92px",
+                marginRight: "12.97px",
+              }
             : null
         }
       ></hr>
       <div className={`price-wrap ${setLeftPadding ? "left-padding" : ""}`}>
-        <p className="p-large-regular">총 금액</p>
-        <p className="p-large-regular">
+        <p className="p-medium-regular sm:p-large-regular">총 금액</p>
+        <p className="p-medium-regular sm:p-large-regular">
           {formatPrice(
-            (buyScript ? scriptPrice : 0) + (buyPerform ? performPrice * performAmount : 0)
+            (buyScript ? scriptPrice : 0) +
+              (buyPerform ? performPrice * performAmount : 0)
           )}
           원
         </p>

9207c8b974a3b7f22b0238ecc477843e0de12caf
Junhyeok <cjh64866@naver.com>
2025-08-31 15:38:55 +0900

    feat: MainVer2 버튼 두 겹씩, 각 메뉴별 글씨 위치 수정

--- src/pages/MainVer2.jsx
+++ src/pages/MainVer2.jsx
@@ -187,7 +187,13 @@ const MainVer2 = () => {
                   ? content2Texts()
                   : content3Texts()}
 
-                <h1 className={page2TitleClassName}>
+                <h1
+                  className={clsx(
+                    page2TitleClassName,
+                    !(isTablet || isMobile || isSmallMobile) &&
+                      "translate-x-[20px]"
+                  )}
+                >
                   {!(isTablet || isMobile || isSmallMobile)
                     ? content2Title()
                     : content3Title()}
@@ -231,11 +237,14 @@ const MainVer2 = () => {
                 ? content3Texts()
                 : content2Texts()}
 
-              <ImageBtn
-                src={page2ButtonImg}
-                alt="->"
-                className="page2-button"
-              />
+              {/* 아마 이미지 export할 때 버튼 포함/버튼 제외로 다르게 불러와진 것 같음. -> 다르게 처리 */}
+              {(isTablet || isMobile || isSmallMobile) && (
+                <ImageBtn
+                  src={page2ButtonImg}
+                  alt="->"
+                  className="page2-button"
+                />
+              )}
             </div>
           </div>
         </div>

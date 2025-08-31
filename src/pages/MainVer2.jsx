f9d4b00fa34a8ff134661fe6849cb8f4d78e8eec
Junhyeok <cjh64866@naver.com>
2025-08-31 17:45:10 +0900

    feat: MainVer2 텍스트 줄바꿈 방지 및 크기 px로 고정

--- src/pages/MainVer2.jsx
+++ src/pages/MainVer2.jsx
@@ -101,7 +101,7 @@ const MainVer2 = () => {
                 <h1 className=" title">
                   당신의 대본이
                   <br />
-                  무대위에 오르기까지
+                  <span className="whitespace-nowrap">무대위에 오르기까지</span>
                 </h1>
 
                 <h5 className=" sub-title whitespace-nowrap">

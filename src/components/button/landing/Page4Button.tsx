55855a95a8b757c3c8a95c163e08cbbea8435d07
Junhyeok <cjh64866@naver.com>
2025-09-16 12:38:52 +0900

    feat: 랜딩페이지 brunch btn 추가

--- src/components/button/landing/Page4Button.tsx
+++ src/components/button/landing/Page4Button.tsx
@@ -4,12 +4,13 @@ interface Page4ButtonProps {
   src: string;
   alt: string;
   onClick: () => void;
+  imgClassName?: string;
 }
 
-const Page4Button: React.FC<Page4ButtonProps> = ({ src, alt, onClick }) => {
+const Page4Button = ({ src, alt, onClick, imgClassName }: Page4ButtonProps) => {
   return (
     <button className="page4-button c-pointer" onClick={onClick}>
-      <img src={src} alt={alt} />
+      <img src={src} alt={alt} className={imgClassName} />
     </button>
   );
 };

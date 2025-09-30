2196ff969ffc339a4aaad62f623636981fb967d4
Junhyeok <cjh64866@naver.com>
2025-09-30 14:31:23 +0900

    feat: 마이페이지 삭제된 작품일 때 UI 및 로직 수정

--- src/components/myPage/ScriptContentPopUp.jsx
+++ src/components/myPage/ScriptContentPopUp.jsx
@@ -1,20 +1,44 @@
+import clsx from "clsx";
 import closeBtn from "../../assets/image/button/CloseBtn.svg";
 
 import "./ScriptContentPopUp.css";
 import "./../../styles/utilities.css";
 
-const ScriptContentPopUp = ({ onClose }) => {
+/**
+ * @param {Object} props - Component properties
+ * @param {string} props.className - Additional CSS classes
+ * @param {function} props.onClose - Callback function to close the popup
+ * @param {string} props.type - Type of the popup ("script" or "account")
+ */
+const ScriptContentPopUp = ({ className, onClose, type }) => {
+  let content;
+  if (type === "script") {
+    content = (
+      <p>
+        작가가 작품을 삭제하여 <br /> 더 이상 공연권을 구매하거나 <br /> 대본을
+        다운받을 수 없습니다.{" "}
+      </p>
+    );
+  } else if (type === "account") {
+    content = (
+      <p>
+        작가가 계정을 삭제하여 <br /> 더 이상 공연권을 구매하거나 <br /> 대본을
+        다운받을 수 없습니다.{" "}
+      </p>
+    );
+  }
+
   return (
-    <div className="script-content-popup">
+    <div className={clsx("script-content-popup", className)}>
       <div className="d-flex j-content-end">
-        <img src={closeBtn} alt="" className="close-button" onClick={onClose}></img>
+        <img
+          src={closeBtn}
+          alt=""
+          className="close-button"
+          onClick={onClose}
+        ></img>
       </div>
-      <div className="d-flex j-content-center">
-        <p>
-          작가가 계정을 삭제하여 <br /> 더 이상 공연권을 구매하거나 <br /> 대본을 다운받을 수
-          없습니다.{" "}
-        </p>
-      </div>
+      <div className="d-flex j-content-center">{content}</div>
     </div>
   );
 };

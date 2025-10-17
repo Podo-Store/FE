9350deeeb6166cf02f1e61b915b8340e050f83cf
Junhyeok <cjh64866@naver.com>
2025-10-17 12:09:45 +0900

    feat: SideMenuDialog 320 화면 추가

--- src/components/navBar/SideDialogBtn.tsx
+++ src/components/navBar/SideDialogBtn.tsx
@@ -1,7 +1,12 @@
 import "./SideDialogBtn.css";
 
 const SideDialogBtn = ({ ...props }) => {
-  return <button className="side-dialog-btn h3-regular t-align-start" {...props}></button>;
+  return (
+    <button
+      className="side-dialog-btn pl-[25px] sm:pl-[50px] w-full h-[50px] sm:h-[86px] p-small-regular sm:h3-regular text-start"
+      {...props}
+    ></button>
+  );
 };
 
 export default SideDialogBtn;

146400d2b468bd7fe7013a388e6ab28cff956d5d
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-11-14 13:43:25 +0900

    fix: RoundBtnV2Props color에 dark_mode type 추가

--- src/components/button/round_btn/types/roundBtnV2Props.ts
+++ src/components/button/round_btn/types/roundBtnV2Props.ts
@@ -1,3 +1,4 @@
-export interface RoundBtnV2Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  color: "white" | "grey" | "purple";
+export interface RoundBtnV2Props
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  color: "white" | "grey" | "purple" | "dark_mode";
 }

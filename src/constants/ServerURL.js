fe6152f7f48d1133b8c12ee091484d0a787c9ec4
goodsmell <87801306+goodsmell@users.noreply.github.com>
2026-06-05 19:16:32 +0900

    feat: SERVER_URL을 환경 변수로 설정하여 API 엔드포인트 관리 개선

--- src/constants/ServerURL.js
+++ src/constants/ServerURL.js
@@ -1,3 +1,1 @@
-export const SERVER_URL = "http://localhost:8080/";
-// export const SERVER_URL = "http://13.125.61.13:8080/";
-// export const SERVER_URL = "https://api.podo-store.com/";
+export const SERVER_URL = import.meta.env.VITE_API_BASE_URL;

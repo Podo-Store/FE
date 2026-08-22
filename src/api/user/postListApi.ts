cf82dca99fe944d4cb2a752af4fabc84f3728ba5
goodsmell <87801306+goodsmell@users.noreply.github.com>
2026-08-22 21:52:20 +0900

    feat: 작품 둘러보기 검색 기능 구현

--- src/api/user/postListApi.ts
+++ src/api/user/postListApi.ts
@@ -1,5 +1,5 @@
 import { AxiosError } from "axios";
-import { api } from "@/api/api";
+import { api, authApi } from "@/api/api";
 
 export interface ScriptItem {
   id: string;
@@ -17,29 +17,62 @@ export interface ScriptItem {
   viewCount: number;
 }
 
-interface PageResponse<T> {
+export interface PageResponse<T> {
   content: T[];
+  pageable: {
+    pageNumber: number;
+    pageSize: number;
+    sort: PageSort;
+    offset: number;
+    paged: boolean;
+    unpaged: boolean;
+  };
   last: boolean;
   totalElements: number;
   totalPages: number;
+  first: boolean;
   number: number;
   size: number;
+  sort: PageSort;
   numberOfElements: number;
   empty: boolean;
 }
 
+interface PageSort {
+  empty: boolean;
+  sorted: boolean;
+  unsorted: boolean;
+}
+
+export type SortType = "POPULAR" | "LIKE_COUNT" | "LATEST";
+export type PlayType = "LONG" | "SHORT";
+
+export interface ExploreScriptsParams {
+  sortType?: SortType;
+  page?: number;
+  size?: number;
+  playType?: PlayType;
+  search?: string;
+}
+
 export const getExploreScripts = async (
-  page: number = 0,
-  sortType: "POPULAR" | "LIKE_COUNT" | "LATEST" = "POPULAR"
-): Promise<ScriptItem[]> => {
+  {
+    page = 0,
+    size = 40,
+    sortType = "POPULAR",
+    playType,
+    search = "",
+  }: ExploreScriptsParams = {}
+): Promise<PageResponse<ScriptItem>> => {
   try {
-    const response = await api.get<PageResponse<ScriptItem>>(`/scripts/v2`, {
-      params: { page, sortType },
+    const response = await authApi.get<PageResponse<ScriptItem>>("/scripts/v2", {
+      params: { page, size, sortType, playType, search },
+      headers: { "Content-Type": "multipart/form-data" },
     });
 
-    return response.data.content;
+    return response.data;
   } catch (error) {
-    console.error("Error fetching long works:", error);
+    console.error("Error fetching explore scripts:", error);
     throw new Error(` 작품 전체 API 호출 실패: ${(error as Error).message}`);
   }
 };
@@ -57,38 +90,6 @@ export const getContestScripts = async (
   } catch (error) {
     console.error("Error fetching contest scripts:", error);
     throw new Error(`공모전 작품 API 호출 실패: ${(error as Error).message}`);
-  }
-};
-
-export const getLongWorks = async (
-  page: number = 0,
-  sortType: "POPULAR" | "LIKE_COUNT" | "LATEST" = "POPULAR"
-): Promise<ScriptItem[]> => {
-  try {
-    const response = await api.get<ScriptItem[]>(`/scripts/long`, {
-      params: { page, sortType },
-    });
-
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching long works:", error);
-    throw new Error(` 장편 작품 API 호출 실패: ${(error as Error).message}`);
-  }
-};
-
-export const getShortWorks = async (
-  page: number = 0,
-  sortType: "POPULAR" | "LIKE_COUNT" | "LATEST" = "POPULAR"
-): Promise<ScriptItem[]> => {
-  try {
-    const response = await api.get<ScriptItem[]>(`/scripts/short`, {
-      params: { page, sortType },
-    });
-
-    return response.data;
-  } catch (error) {
-    console.error("Error fetchin short works:", error);
-    throw new Error(`단편 작품 API 호출 실패: ${(error as Error).message}`);
   }
 };
 
